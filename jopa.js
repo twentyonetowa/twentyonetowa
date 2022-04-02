906939f46e065c09220c0dc38cb64e1060ae1723
21towa <98212597+twentyonetowa@users.noreply.github.com>
2022-04-02 17:09:14 +0500

    Create jopa.js

---
+++ jopa.js
@@ -0,0 +1,12 @@
+using System;
+
+namespace Programm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Putin!");
+        }
+    }
+}
